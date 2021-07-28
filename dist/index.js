d3ece21a63d0dc50e79ef5d909a4998627de90d4
leidenglai <leidenglai@qq.com>
2021-07-28 11:50:59 +0800

    relese: v0.0.5

---
+++ dist/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var pluginPlatformAlipay = require('@tarojs/plugin-platform-alipay');
+var shared = require('@tarojs/shared');
+
+const components = {
+    // ======== 额外组件 ========
+    OpenAvatar: {
+        'userId': '',
+        'openId': '',
+        'nickName': '',
+        'avatar': '',
+        'size': shared.singleQuote('normal')
+    }
+};
+
+const PACKAGE_NAME = '@tarojs/plugin-platform-alipay-dd';
+class DD extends pluginPlatformAlipay.Alipay {
+    /**
+     * 1. setupTransaction - init
+     * 2. setup
+     * 3. setupTransaction - close
+     * 4. buildTransaction - init
+     * 5. build
+     * 6. buildTransaction - close
+     */
+    constructor(ctx, config) {
+        super(ctx, config);
+        this.platform = 'dd';
+        this.runtimePath = `${PACKAGE_NAME}/dist/runtime`;
+        this.taroComponentsPath = `${PACKAGE_NAME}/dist/components-react`;
+        this.setupTransaction.addWrapper({
+            close: this.modifyTemplate
+        });
+    }
+    /**
+     * 增加组件或修改组件属性
+     */
+    modifyTemplate() {
+        // 处理钉钉与支付宝的组件差异
+        this.template.mergeComponents(this.ctx, components);
+    }
+}
+
+var index = (ctx) => {
+    ctx.registerPlatform({
+        name: 'dd',
+        useConfigName: 'mini',
+        async fn({ config }) {
+            const program = new DD(ctx, config);
+            await program.start();
+        }
+    });
+};
+
+exports.default = index;
+//# sourceMappingURL=index.js.map
---
+++ dist/runtime.js
@@ -0,0 +1,59 @@
+import { processApis, singleQuote, mergeReconciler, mergeInternalComponents } from '@tarojs/shared';
+import { noPromiseApis, needPromiseApis, handleSyncApis, transformMeta, modifyAsyncResult, request, hostConfig, components as components$1 } from '@tarojs/plugin-platform-alipay/dist/runtime-utils';
+
+const syncApis = new Set([
+    'previewFileInDingTalk',
+]);
+const asyncApis = new Set([
+    'editPicture',
+    'enableLeaveConfirm',
+    'disableLeaveConfirm',
+    'complexChoose',
+    'chooseDepartments',
+    'createGroupChat',
+    'choosePhonebook',
+    'chooseExternalUsers',
+    'editExternalUser',
+    'chooseUserFromList',
+    'createDing',
+    'callUsers',
+    'showCallMenu',
+    'checkBizCall',
+    'pay',
+    'saveFileToDingTalk',
+    'uploadAttachmentToDingTalk',
+    'chooseDingTalkDir',
+    'chooseChatForNormalMsg',
+    'chooseChat',
+    'openChatByChatId',
+    'openChatByUserId'
+]);
+
+function initNativeApi(taro) {
+    processApis(taro, dd, {
+        noPromiseApis: new Set([...noPromiseApis, ...syncApis]),
+        needPromiseApis: new Set([...needPromiseApis, ...asyncApis]),
+        handleSyncApis,
+        transformMeta,
+        modifyAsyncResult,
+        request
+    });
+    taro.SDKVersion = dd.SDKVersion;
+}
+
+const components = {
+    // ======== 额外组件 ========
+    OpenAvatar: {
+        'userId': '',
+        'openId': '',
+        'nickName': '',
+        'avatar': '',
+        'size': singleQuote('normal')
+    }
+};
+
+hostConfig.initNativeApi = initNativeApi;
+mergeReconciler(hostConfig);
+mergeInternalComponents(components$1);
+mergeInternalComponents(components);
+//# sourceMappingURL=runtime.js.map
